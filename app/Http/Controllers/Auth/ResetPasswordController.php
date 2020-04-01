106d90433f2c1a08b8e71c34658f58264b976222
vagrant <vagrant@homestead>
2020-04-01 08:21:22 +0000

    编辑和显示个人资料

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,7 +26,8 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = "/";
 
     public function __construct()
     {       {
